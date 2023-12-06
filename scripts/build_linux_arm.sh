825e3ed4abf042b50e07d478e3ed9474aba7bf2b
joseph-sentry <136376984+joseph-sentry@users.noreply.github.com>
2023-12-06 11:33:37 -0500

    test

--- scripts/build_linux_arm.sh
+++ scripts/build_linux_arm.sh
@@ -5,6 +5,5 @@ pip install .
 python setup.py build
 STATICCODECOV_LIB_PATH=$(find build/ -maxdepth 1 -type d -name 'lib.*' -print -quit | xargs -I {} sh -c "find {} -type f -name 'staticcodecov*' -print -quit | sed 's|^./||'")`
 pyinstaller --add-binary ${STATICCODECOV_LIB_PATH}:. --copy-metadata codecov-cli --hidden-import staticcodecov_languages -F codecov_cli/main.py
-echo $1
-echo $2
-cp ./dist/main ./dist/codecovcli_$1
+echo $*
+cp ./dist/main ./dist/codecovcli_$*
